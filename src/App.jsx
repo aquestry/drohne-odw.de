21732fc50754c164898c953bdc448bcdfb121553
aquestry <155507142+aquestry@users.noreply.github.com>
2025-01-13 18:42:10 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
 function App() {
-  return (
-      <>
-          <object data="header.svg" size="500"></object>
-          <a href="https://schmid-koenig.de/impressum-n/">
-              Impressum
-          </a>
-      </>
-  )
+    return (
+        <>
+            <div className="header">
+                <img src="header.svg" alt="header" className="header-icon" />
+                <button onClick={() => window.location.href = 'https://schmid-koenig.de/impressum-n/'}>Impressum</button>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
