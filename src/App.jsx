9443bc176b82ee99faeafb0352923396a6e23bbe
aquestry <antonkonig691@gmail.com>
2025-02-18 23:28:38 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import React from 'react';
+
 function App() {
-    return (
-        <>
-            <div className="header">
-                <img src="header.svg" alt="header" className="header-icon" />
-                <button onClick={() => window.location.href = 'https://schmid-koenig.de/impressum-n/'}>Impressum</button>
-            </div>
-        </>
-    );
+  return (
+    <div className="header">
+      <img src="/header.svg" alt="Header" className="header-icon" />
+      <button onClick={() => window.location.href = 'https://schmid-koenig.de/impressum-n/'}>
+        Impressum
+      </button>
+    </div>
+  );
 }
+
 export default App;
